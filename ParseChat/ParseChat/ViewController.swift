4094552984670f1a90183b2eeaa092b7e78cec72
Kristine Laranjo <kristine@Kristines-MacBook-Pro.local>
2018-02-21 14:29:45 -0800

    Initialize repo

---
+++ ParseChat/ParseChat/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ParseChat
+//
+//  Created by Kristine Laranjo on 2/21/18.
+//  Copyright © 2018 Kristine Laranjo. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
